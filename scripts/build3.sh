f78a987b5fe349c162fdd47b2a23ba6116eaf626
lebao3105 <bao12345yocoo@gmail.com>
2022-06-11 19:58:54 +0700

    Install man-pages & iana-etc

--- scripts/build3.sh
+++ scripts/build3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is not tested yet,
+# This script is not completed yet,
 # and it based on LFS chapter 8, which builds a lot of packages.
 # Part 1 - build man-pages to mpfr.
 
@@ -29,3 +29,19 @@ checkpkg gmp
 checkpkg mpfr
 
 check_chroot
+
+function ins_man() {
+    cd $LFS/sources/
+    rm -rf man-pages-5.13
+    tar -xf man-pages-5.13.tar.xz
+    cd man-pages-5.13
+    make prefix=/usr install
+}
+
+function ins_ianaetc() {
+    cd $LFS/sources/ && rm -rf iana-etc-20220524
+    tar -xf iana-etc-20220524.tar.xz
+    cd iana-etc-20220524
+    cp services protocols /etc
+}
+}
